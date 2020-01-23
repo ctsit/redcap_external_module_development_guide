7db32ea8e65f1174f1b76a92eb4cf65df7a633e4
Kyle Chesney <kyle.2493@gmail.com>
2020-01-23 16:00:34 -0500

    adjust slashes in directories to mirror config.yml

--- docs/script/serve.sh
+++ docs/script/serve.sh
@@ -2,7 +2,7 @@
 # set those variables in _config_local.yaml. E.g. add a line like this to 
 # link to the root of the develop branch of the CTSIT fork:
 #
-#   repo_root: https://github.com/ctsit/redcap_external_module_development_guide/tree/develop
+#   repo_root: https://github.com/ctsit/redcap_external_module_development_guide/tree/develop/
 #
 # This script will load the contents of _config_local.yaml last if it exists.
 # Github will only reference _config.yaml when building the site.
